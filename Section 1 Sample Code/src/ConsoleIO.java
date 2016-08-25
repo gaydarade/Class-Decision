063d3a79279db5bd14e08bd63d8fed3847c0c9b3
Damon E. Drake <damon.e.drake@gmail.com>
2016-08-25 14:34:10 -0500

    Made changes to sample 1 ConsoleIO.java

--- Section 1 Sample Code/src/ConsoleIO.java
+++ Section 1 Sample Code/src/ConsoleIO.java
@@ -1,9 +1,23 @@
+/*
+  Scanner attaches to the keyboard using System.in
+  The following import statement is required to use the scanner
+*/
+import java.util.Scanner;
 
 public class ConsoleIO {
 
+	public static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.print("Enter in First Name: ");
+		String firstName = scanner.nextLine();
+		
+		System.out.print("Enter in age: ");
+		int age = Integer.parseInt(scanner.nextLine());
+		
+		// Close the scanner before ending the entire application to prevent IO handle attachment issues.
+		scanner.close(); 
 	}
 
 }
