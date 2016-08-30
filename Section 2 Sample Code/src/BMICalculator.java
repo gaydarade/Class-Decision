38316f74028a171b1de5d07093e12df31cc58643
gaydarade <gaydaradejade@gmail.com>
2016-08-30 10:10:06 -0500

    Updated BMICalculator.java to properly display BMI classifications.

--- Section 2 Sample Code/src/BMICalculator.java
+++ Section 2 Sample Code/src/BMICalculator.java
@@ -11,6 +11,7 @@
 	30.0 or more	Obese
 	-----------------------------------
  */
+import java.text.Format;
 import java.util.Scanner;
 
 public class BMICalculator {
@@ -22,16 +23,21 @@ public class BMICalculator {
 		//TODO Determine which category the person belongs to
 		//TODO Print out BMI and Category
 		
-		System.out.print("Height (meters):");
+		System.out.print("Height (meters): ");
 		double h = Double.parseDouble(in.nextLine());
-		System.out.print("Weight (kilos):");
+		System.out.print("Weight (kilos): ");
 		double w = Double.parseDouble(in.nextLine());
 		
-		double bmi = w / (h*h);
-		
-		switch bmi 
-		}
-		
+		double bmi =  w / (h*h);
+		System.out.println("Your specific BMI is " + bmi + ".");
+		String m = "";
+		if (bmi <= 18.5 && bmi >= 0) { m = "under weight"; }
+		else if (bmi >= 18.5 && bmi <= 24.9) { m = "normal"; }
+		else if (bmi >= 25.0 && bmi <= 29.9) { m = "overweight"; }
+		else if (bmi < 0) { m = "n/a"; }
+		else { m = "obese"; }
+		// ASK DAMON ABOUT DECIMAL FORMATS
+		System.out.print("Your BMI qualifies you as " + m + ".");
 	}
 }
 
