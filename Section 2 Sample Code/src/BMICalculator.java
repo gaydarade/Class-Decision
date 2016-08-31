0c9e12057ca8170b0162512fc17b09dcc9b6ea9f
Jaderade <gaydaradejade@gmail.com>
2016-08-31 16:45:15 -0500

    Added DecimalFormat to clean up some BMI display messages.

--- Section 2 Sample Code/src/BMICalculator.java
+++ Section 2 Sample Code/src/BMICalculator.java
@@ -1,43 +1,25 @@
-/*  It is computed by taking the individual's weight (mass) in kilograms and dividing it by the 
-	square of their height in meters.
-	
-	Use the 4 categories below to to determine the output.
-	
-	BMI				Category
-	-----------------------------------
-	< 18.5			Under Weight
-	18.5 - 24.9		Normal
-	25.0 - 29.9		Overweight
-	30.0 or more	Obese
-	-----------------------------------
- */
-import java.text.Format;
 import java.util.Scanner;
-
+import java.text.DecimalFormat;
 public class BMICalculator {
 	public static Scanner in = new Scanner(System.in);
 	public static void main(String[] args) {
-		//TODO Prompt for person's height in meters
-		//TODO Prompt for person's weight in kilograms
-		//TODO Calculate the person's BMI
-		//TODO Determine which category the person belongs to
-		//TODO Print out BMI and Category
 		
 		System.out.print("Height (meters): ");
 		double h = Double.parseDouble(in.nextLine());
 		System.out.print("Weight (kilos): ");
 		double w = Double.parseDouble(in.nextLine());
 		
-		double bmi =  w / (h*h);
+		double b =  w / (Math.pow(h, 2));
+		DecimalFormat df = new DecimalFormat("#.0"); 
+		String bmi = df.format(b);
 		System.out.println("Your specific BMI is " + bmi + ".");
 		String m = "";
-		if (bmi <= 18.5 && bmi >= 0) { m = "under weight"; }
-		else if (bmi >= 18.5 && bmi <= 24.9) { m = "normal"; }
-		else if (bmi >= 25.0 && bmi <= 29.9) { m = "overweight"; }
-		else if (bmi < 0) { m = "n/a"; }
-		else { m = "obese"; }
-		// ASK DAMON ABOUT DECIMAL FORMATS
-		System.out.print("Your BMI qualifies you as " + m + ".");
+		if (b <= 18.5 && b >= 0) { m = "under weight"; }
+		else if (b >= 18.5 && b <= 24.9) { m = "normal"; }
+		else if (b >= 25.0 && b <= 29.9) { m = "overweight"; }
+		else if (b < 0) { m = "n/a"; } else
+			m = "obese";
+		System.out.print("This classifies you as " + m + ".");
 	}
 }
 
