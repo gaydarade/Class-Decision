e3939674cf485160f7e093ebaf99e43c0b6965e9
gaydarade <gaydaradejade@gmail.com>
2016-09-22 10:15:41 -0500

    MAGNETIC STRIPE MORE LIKE GAGNETIC STRIPE AMIRITE?

--- Section 3 Sample Code/src/Homework/MagneticStripe.java
+++ Section 3 Sample Code/src/Homework/MagneticStripe.java
@@ -23,13 +23,20 @@ import java.util.ArrayList;
 public class MagneticStripe {
 	public static void main(String[] args) {
 		List<String> tracks = new ArrayList<String>();
-		
 		tracks.add("%B6011000000000012^DRAKE/DAMON E             ^18071X100000*000000000XXX000000?");
 		tracks.add("%B300012552615478^DUCK/DONALD               ^1806Z3011225*000378000XXX000142?");
-
+		List<MagneticStripe> stripes = new ArrayList<MagneticStripe>();
+		
+		for (String t : tracks) {
+			MagneticStripe ms = new MagneticStripe(t);
+			stripes.add(ms);
+		}
+		
+		for (MagneticStripe ms : stripes) {
+			ms.print();
+		}
 	}
 	
-	/*--- CLASS INSTANCE CODE ---*/
 	private String accountNumber;
 	public String getAccountNumber() { return accountNumber; }
 	public void setAccountNumber(String an) { accountNumber = an; }
@@ -37,11 +44,40 @@ public class MagneticStripe {
 	private String expYear;
 	public String getExpYear() { return expYear; }
 	public void setExpYear(String ey) { expYear = ey; }
+	
 	private String expMonth;
+	public String getExpMonth() { return expMonth; }
+	public void setExpMonth(String em) { expMonth = em; }
+	
 	private String firstName;
+	public String getFirstName() { return firstName; }
+	public void setFirstName(String fn) { firstName = fn; }
+	
 	private String lastName;
+	public String getLastName() { return lastName; }
+	public void setLastName(String ln) { lastName = ln; }
 	
 	public void print() {
+		System.out.println("Acct #: " + getAccountNumber() + "\n" +
+						"First Name: " + getFirstName() + "\n" +
+						"Last Name: " +  getLastName() + "\n" +
+						"Exp Month: " + getExpMonth() + " Exp Year: " + getExpYear() + "\n");
+	}
+	
+	public MagneticStripe(String trk) {
+		String[] array = trk.split("\\^");
+		String acctNum = array[0].substring(2,array[0].length()).trim();
+		String[] names = array[1].split("\\/");
+		String first = names[1].trim();
+		String last = names[0].trim();
+		String year = array[2].substring(0,2);
+		String month = array[2].substring(2,4);
+
+		setAccountNumber(acctNum);
+		setFirstName(first);
+		setLastName(last);
+		setExpYear(year);
+		setExpMonth(month);
 		
 	}
 }
